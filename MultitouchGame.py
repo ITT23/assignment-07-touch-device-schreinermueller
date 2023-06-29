ca6090ab41ee211cc348ac923cde00db3661f26a
m_uellermichael <m_uellermichael@gmx.de>
2023-06-29 22:37:59 +0200

    random orientation and size + move works for all orientations

--- MultitouchGame.py
+++ MultitouchGame.py
@@ -1,22 +1,115 @@
 import os
+import random
+
 import pyglet
+from math import cos, sin, radians
+
+
+# https://stackoverflow.com/questions/36399381/whats-the-fastest-way-of-checking-if-a-point-is-inside-a-polygon-in-python
+def point_in_poly(x, y, poly):
+    n = len(poly)
+    inside = False
+    p2x = 0.0
+    p2y = 0.0
+    xints = 0.0
+    p1x, p1y = poly[0]
+    for i in range(n+1):
+        p2x, p2y = poly[i % n]
+        if y > min(p1y, p2y):
+            if y <= max(p1y, p2y):
+                if x <= max(p1x, p2x):
+                    if p1y != p2y:
+                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xints:
+                        inside = not inside
+        p1x, p1y = p2x, p2y
+
+    return inside
 
 class MultitouchGame:
     def __init__(self):
         # get all images
+        self.sprite_corners = []
         stairsImagePath = os.path.normpath("img/stairs.jpg")
-        self.stairsSprite = pyglet.sprite.Sprite(img=pyglet.image.load(stairsImagePath), x=100, y=50)
-        self.stairsSprite.scale = 0.3
+        self.stairsSprite = pyglet.sprite.Sprite(img=pyglet.image.load(stairsImagePath), x=300, y=50)
+        self.stairsSprite.scale = random.uniform(0.15, 0.5)
+        self.stairsSprite.poly = []
+        self.stairsSprite.rotation = random.randrange(0, 360, 1)
 
         tablesImagePath = os.path.normpath("img/tables.jpg")
-        self.tablesSprite = pyglet.sprite.Sprite(img=pyglet.image.load(tablesImagePath), x=650, y=30)
-        self.tablesSprite.scale = 0.3
+        self.tablesSprite = pyglet.sprite.Sprite(img=pyglet.image.load(tablesImagePath), x=1000, y=250)
+        self.tablesSprite.scale = random.uniform(0.15, 0.5)
+        self.stairsSprite.poly = []
+        self.tablesSprite.rotation = random.randrange(0, 360, 1)
 
         windowsImagePath = os.path.normpath("img/windows.jpg")
-        self.windowsSprite = pyglet.sprite.Sprite(img=pyglet.image.load(windowsImagePath), x=300, y=400)
-        self.windowsSprite.scale = 0.3
+        self.windowsSprite = pyglet.sprite.Sprite(img=pyglet.image.load(windowsImagePath), x=800, y=400)
+        self.windowsSprite.scale = random.uniform(0.15, 0.5)
+        self.stairsSprite.poly = []
+        self.windowsSprite.rotation = random.randrange(0, 360, 1)
+
+        self.images = [self.stairsSprite, self.windowsSprite, self.tablesSprite]
+
 
     def draw(self):
         self.stairsSprite.draw()
         self.tablesSprite.draw()
         self.windowsSprite.draw()
+
+    # check if finger is in an image
+    def in_image(self, point):
+        x, y = point
+        for image in self.images:
+            # image.get_transformed_vertices()
+            if image.x <= x <= image.x + image.width and image.y <= y <= image.y + image.height:
+                return image
+        return None
+
+    def move(self, sprite, dist_x, dist_y):
+        sprite.x = sprite.x + dist_x
+        sprite.y = sprite.y + dist_y
+
+    def selected_sprite(self, x, y):
+        images = [self.stairsSprite, self.tablesSprite, self.windowsSprite]
+        for image in images:
+            if point_in_poly(x, y, image.poly):
+                return image
+        return None
+
+    # get all rotated corners
+    def get_sprite_corners(self):
+        if len(self.sprite_corners) == 3:
+            self.sprite_corners = []
+
+        for image in self.images:
+            # chatGPT
+            # Ursprüngliche Eckpunkte des Sprites
+            x1 = image.x
+            y1 = image.y
+            x2 = x1 + image.width
+            y2 = y1
+            x3 = x1 + image.width
+            y3 = y1 + image.height
+            x4 = x1
+            y4 = y1 + image.height
+
+            angle = -radians(image.rotation)
+
+            # rotating around upper left corner
+            cx = x1
+            cy = y1
+            # rotated corners
+            rotated_x1 = cx + (x1 - cx) * cos(angle) - (y1 - cy) * sin(angle)
+            rotated_y1 = cy + (x1 - cx) * sin(angle) + (y1 - cy) * cos(angle)
+            rotated_x2 = cx + (x2 - cx) * cos(angle) - (y2 - cy) * sin(angle)
+            rotated_y2 = cy + (x2 - cx) * sin(angle) + (y2 - cy) * cos(angle)
+            rotated_x3 = cx + (x3 - cx) * cos(angle) - (y3 - cy) * sin(angle)
+            rotated_y3 = cy + (x3 - cx) * sin(angle) + (y3 - cy) * cos(angle)
+            rotated_x4 = cx + (x4 - cx) * cos(angle) - (y4 - cy) * sin(angle)
+            rotated_y4 = cy + (x4 - cx) * sin(angle) + (y4 - cy) * cos(angle)
+
+            image.poly = [(rotated_x1, rotated_y1), (rotated_x2, rotated_y2), (rotated_x3, rotated_y3),
+                          (rotated_x4, rotated_y4)]
+            self.sprite_corners.append([(rotated_x1, rotated_y1), (rotated_x2, rotated_y2), (rotated_x3, rotated_y3),
+                                        (rotated_x4, rotated_y4)])
+
--- multitouch-demo.py
+++ multitouch-demo.py
@@ -21,6 +21,7 @@ touch = pyglet.shapes.Circle(0, 0, 5, color=(255, 0, 0))
 finger_one = pyglet.shapes.Circle(0, 0, 5, color=(0, 255, 0))
 finger_two = pyglet.shapes.Circle(0, 0, 5, color=(0, 255, 0))
 
+sprite_corners = []
 
 
 def handle_sensordata(data):
@@ -30,19 +31,10 @@ def handle_sensordata(data):
 sensor.register_callback('events', handle_sensordata)
 
 
-#check if finger is in an image
-def in_image(point):
-    images = [game.stairsSprite, game.tablesSprite, game.windowsSprite]
-    x, y = point
-    for image in images:
-        if image.x <= x <= image.x + image.width and image.y <= y <= image.y + image.height:
-            return image
-    return None
-
-
 def update(dt):
     global move_array
     global resize_rotate_array
+    game.get_sprite_corners()
 
     if len(sensor.get_value('events')) > 0:
         # move
@@ -54,7 +46,7 @@ def update(dt):
             y = int(window.height - sensor_y * window.height)
             touch.x = x
             touch.y = y
-            sprite = in_image((x, y))
+            sprite = game.selected_sprite(x, y)
             dist_x = 0
             dist_y = 0
             if len(move_array) == 0:
@@ -66,10 +58,9 @@ def update(dt):
                 move_array.pop(0)
 
             if sprite is not None:
-                #print(dist_x, dist_y)
                 # hier quasi funktion für move
-                sprite.x = sprite.x + dist_x
-                sprite.y = sprite.y + dist_y
+                game.move(sprite, dist_x, dist_y)
+
             else:
                 move_array = []
         # resize or rotate
@@ -81,7 +72,7 @@ def update(dt):
                 x = int(sensor_x * window.width)
                 y = int(window.height - sensor_y * window.height)
                 finger_coordinates.append((x, y))
-            #print(finger_coordinates)
+
             resize_rotate_array.append(finger_coordinates)
             finger_one.x = finger_coordinates[0][0]
             finger_one.y = finger_coordinates[0][1]
@@ -98,8 +89,8 @@ def update(dt):
                 finger_two_x_2, finger_two_y_2 = finger_coordinates_2[1]
                 scale_factor_1 = abs(finger_one_x_1 - finger_two_x_1) / window.width
                 scale_factor_2 = abs(finger_one_x_2 - finger_two_x_2) / window.width
-                if in_image(finger_coordinates_1[0]) and in_image(finger_coordinates_2[0]) and in_image(finger_coordinates_1[1]) and in_image(finger_coordinates_2[1]) is not None:
-                    sprite = in_image(finger_coordinates_1[0])
+                if game.in_image(finger_coordinates_1[0]) and game.in_image(finger_coordinates_2[0]) and game.in_image(finger_coordinates_1[1]) and game.in_image(finger_coordinates_2[1]) is not None:
+                    sprite = game.in_image(finger_coordinates_1[0])
                     print("scale FAKTOR: " + str(scale_factor_2)) #abziehen von der eigentlichen scale glaub ich
                     sprite.scale = scale_factor_2
                     resize_rotate_array.pop(0)
@@ -107,12 +98,11 @@ def update(dt):
                     resize_rotate_array = []
 
 
-
-
 # # fullscreen window
 # window = pyglet.window.Window(fullscreen=True)
 
 game = MultitouchGame()
+
 
 @window.event
 def on_draw():
@@ -121,12 +111,23 @@ def on_draw():
     touch.draw()
     finger_one.draw()
     finger_two.draw()
+    for i in range(3):
+        for point in game.sprite_corners[i]:
+            x, y = point
+            radius = 10  # Radius des Kreises
+
+            # Kreisfarbe festlegen
+            circle_color = (255, 0, 0)  # Rot (RGB-Werte)
+
+            # Kreis zeichnen
+            pyglet.shapes.Circle(x, y, radius, color=circle_color).draw()
 
 
 @window.event
 def on_key_press(symbol, modifiers):
     if symbol == pyglet.window.key.Q:
         sys.exit(0)
+
 
 # init of DIPPID-sender and touch-detector
 touchInput = TouchInput()
